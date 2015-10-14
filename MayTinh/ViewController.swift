b864a0c381bf2df6fbf7cdfe6e72a90f8d4f9c2c
phuoc <maithanhphuoc1991@gmail.com>
2015-10-14 15:43:19 +0700

    Initial Commit

---
+++ MayTinh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MayTinh
+//
+//  Created by phuoc on 10/14/15.
+//  Copyright © 2015 phuocmt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
